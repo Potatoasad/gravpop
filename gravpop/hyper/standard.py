numerical stability fixes to allow for gradient evaluation

gravpop/hyper/standard.py
@@ -1,11 +1,13 @@
 import jax
 import jax.numpy as jnp
+from jax import jit
 from ..models.utils import *
 
 
 from dataclasses import dataclass, field
 from typing import List, Dict
 from functools import partial
+
 
 @dataclass
 class PopulationLikelihood:
@@ -15,10 +17,11 @@ class PopulationLikelihood:
     
     def __post_init__(self):
         self.N_events = self.event_data['prior'].shape[0]
+        self.log_epsilon = 1e-30
     
     @staticmethod
     def log(x):
-        return jnp.log(x)
+        return jnp.log(x + 1e-30)
     
     def sampled_event_bayes_factors(self, data, params):
         if len(self.models) == 0:

gravpop/models/mass/mass.py
@@ -29,6 +29,7 @@ class SmoothedTwoComponentPrimaryMassRatio(AbstractPopulationModel):
         self.m1s = jnp.linspace(mmin_fixed, mmax_fixed, normalization_shape[0])
         self.qs = jnp.linspace(0, 1, normalization_shape[1])
         self.m1s_grid, self.qs_grid = jnp.meshgrid(self.m1s, self.qs)
+        self.mass_ratio_norm_clip_threshold = 1e-16
         #print("""Note: SmoothedTwoComponentPrimaryMassRatio is an unnormalized distribution. 
         #    Be wary when using these for infering merger rates. 
         #    In addition, this model might have a different primary mass marginal due to this lack of normalization in q""")
@@ -45,7 +46,8 @@ class SmoothedTwoComponentPrimaryMassRatio(AbstractPopulationModel):
             jnp.ones(self.m1s.shape),
         )
 
-        return jnp.interp(data[self.primary_mass_name], self.m1s, norms)
+        result = jnp.interp(data[self.primary_mass_name], self.m1s, norms)
+        return jnp.clip(result, self.mass_ratio_norm_clip_threshold)  # For preventing 1/0 at masses that are below mmin
 
     def norm_p_m1(self, delta_m, **kwargs):
         """Calculate the normalisation factor for the primary mass"""
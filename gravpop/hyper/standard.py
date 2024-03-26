Made the 'numerically stable' have a smaller epsilon, fixed bug in plotting aspect ratio

gravpop/hyper/standard.py
@@ -48,7 +48,7 @@ class PopulationLikelihood:
     
     @staticmethod
     def log(x):
-        return jnp.log(x + 1e-30)
+        return jnp.log(x + 1e-37)
 
     def sampled_compute_log_weights(self, data, params):
         return sum(self.log(model(data, params)) for model in self.models) - self.log(data["prior"])

gravpop/plots/mass.py
@@ -79,7 +79,9 @@ class MassPlot:
         ax.grid(True, which='major', linestyle='dotted', linewidth='0.5', color='gray', alpha=0.5)
         ax.set_xlim(mass_x.min(), mass_x.max())
         ax.set_ylim(bottom=10**(lowest))
-        ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        new_aspect = aspect*(high_x - low_x)/(highest-lowest)
+        if (new_aspect > 0) and not (np.isinf(new_aspect)):
+            ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
 
         return ax
     
@@ -117,7 +119,10 @@ class MassPlot:
         ax.set_xlim(mass_x.min(), mass_x.max())
         ax.set_yscale("log")
         ax.set_ylim(bottom=10**(log_lower))
-        ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        new_aspect = aspect*(high_x - low_x)/(highest-lowest)
+        if (new_aspect > 0) and not (np.isinf(new_aspect)):
+            ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        #ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
         #ax.set_aspect(aspect*(1)/(highest-lowest))
         
         return ax

gravpop/plots/redshift.py
@@ -70,7 +70,10 @@ class RedshiftPlot:
         second_lowest = np.partition(redshift_x, 1)[1]  
         ax.set_xlim(second_lowest, redshift_x.max())
         ax.set_ylim(bottom=10**(log_lower))
-        ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        #ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        new_aspect = aspect*(high_x - low_x)/(highest-lowest)
+        if (new_aspect > 0) and not (np.isinf(new_aspect)):
+            ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
         
         return fig
     
@@ -99,6 +102,9 @@ class RedshiftPlot:
         second_lowest = np.partition(redshift_x, 1)[1]  
         ax.set_xlim(second_lowest, redshift_x.max())
         ax.set_ylim(bottom=10**(log_lower))
-        ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        #ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        new_aspect = aspect*(high_x - low_x)/(highest-lowest)
+        if (new_aspect > 0) and not (np.isinf(new_aspect)):
+            ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
         
         return fig

gravpop/plots/spin.py
@@ -102,7 +102,10 @@ class SpinMagintudePlot:
         ax.grid(True, which='major', linestyle='dotted', linewidth='0.5', color='gray', alpha=0.5)
         ax.set_xlim(spin_a.min(), spin_a.max())
         #ax.set_ylim(bottom=10**(lowest))
-        ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        #ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
+        new_aspect = aspect*(high_x - low_x)/(highest-lowest)
+        if (new_aspect > 0) and not (np.isinf(new_aspect)):
+            ax.set_aspect(aspect*(high_x - low_x)/(highest-lowest))
         #ax.set_aspect(aspect)
 
         return ax